6085d279e5fe5a4f7f0847042dc98ae4f8e6796b
Cyprien Le Pannérer <cyplp@free.fr>
2019-06-19 11:28:43 +0200

    test on headers

--- src/napstruct/napheader.rs
+++ src/napstruct/napheader.rs
@@ -10,3 +10,17 @@ impl Header {
         Header { name, value }
     }
 }
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let h = Header::new("name".to_string(), "value".to_string());
+        assert_eq!(h.name, "name");
+        assert_eq!(h.value, "value");
+    }
+
+}
